b5bbaef0e7ab8caf9ca25149a61122a534593e25
Amirdharajan M <amirtharajan1990@gmail.com>
2020-07-30 21:52:41 +0530

    Completed

---
+++ ARTICLE/apps/model/__init__.py
@@ -0,0 +1,6 @@
+from .ArticleModel import Article
+from .CommentModel import Comments
+from .CommonCommentModel import CommonComments
+from .LoginHistoryModel import LoginHistory
+from .LikeArticleModel import LikeArticle
+from .CaregoryModel import Category
